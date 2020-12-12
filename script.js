2f86e284516c8a3891137e8ad57df4cfd0b1d683
David Greene <daone764@gmail.com>
2020-12-12 14:08:39 -0500

    added git ignore for modules

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var _ = require('lodash');
 
 
-var array = [1,2,3,4,5,6,7,8];
+//var array = [1,2,3,4,5,6,7,8];
 
 
-var withouttest = console.log('answer:',_.without(array,3));
-alert(withouttest);
+//var withouttest = console.log('answer:',_.without(array,3));
 var css = document.querySelector("h3")
 var color1= document.querySelector(".color1");
 var color2= document.querySelector(".color2");
